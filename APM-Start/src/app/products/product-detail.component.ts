81d15cef6740ed6030ab0841b72502e89660db6d
David Rivas <david.rivas@pevaar.com>
2019-09-11 09:24:14 -0500

    Changes from sept 11

--- APM-Start/src/app/products/product-detail.component.ts
+++ APM-Start/src/app/products/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { IProduct } from './product';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
     //selector: 'pm-product-detail',
@@ -7,12 +8,29 @@ import { IProduct } from './product';
     styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
-    pageTitle: string = ' Product Detail';
+    pageTitle: string = 'Product Detail';
     product: IProduct;
 
-    constructor() {}
+    constructor(private route: ActivatedRoute,
+        private router: Router) {}
 
     ngOnInit() {
+        //+ is to convert string to number
+        let id = +this.route.snapshot.paramMap.get('id');
+        this.pageTitle += `: ${id}`;
+        this.product = {
+            'productId': id,
+            'productName': 'Leaf Rake',
+            'productCode': 'GDN-0011',
+            'releaseDate': 'March 19, 2019',
+            'description': 'Leaf rake with 48-inch wooden handle',
+            'price': 19.95,
+            'starRating': 3.2,
+            'imageUrl': 'assets/images/leaf_rake'
+        };
+    }
 
+    onBack(): void {
+        this.router.navigate(['/products']);
     }
 }
